518cd13849067ad12468107a075c30c7c399d899
tiago <ti.almeida58@gmail.com>
2026-01-21 09:30:13 -0300

    test: Update Spring Boot application tests

---
+++ src/test/java/br/com/mikrotik/MikrotikApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mikrotik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikrotikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
