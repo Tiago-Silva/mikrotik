a35354c3a1411be077b6ec52c8e3ce48aad21b66
tiago <ti.almeida58@gmail.com>
2026-01-21 09:16:09 -0300

    feat: Add Spring Boot application entry point

---
+++ src/main/java/br/com/mikrotik/MikrotikApplication.java
@@ -0,0 +1,13 @@
+package br.com.mikrotik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikrotikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MikrotikApplication.class, args);
+    }
+
+}
