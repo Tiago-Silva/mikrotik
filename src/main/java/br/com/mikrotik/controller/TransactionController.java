fed259103a5c7a4502fd70cde44338bab70af36f
tiago <ti.almeida58@gmail.com>
2026-01-27 14:45:08 -0300

    refactor: remove unused import statement

--- src/main/java/br/com/mikrotik/controller/TransactionController.java
+++ src/main/java/br/com/mikrotik/controller/TransactionController.java
@@ -20,7 +20,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
